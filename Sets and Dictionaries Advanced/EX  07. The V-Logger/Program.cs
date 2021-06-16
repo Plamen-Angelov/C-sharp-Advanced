7843c74249c51858da2abd0eb69eb02adda0c1ad
Plamen Angelov <plamen565@gmail.com>
2021-06-16 11:19:05 +0300

    Sets and Dictionaries Advanced
    Exersice

---
+++ Sets and Dictionaries Advanced/EX  07. The V-Logger/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Diagnostics.Tracing;
+using System.Linq;
+
+namespace EX__07._The_V_Logger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> followersByUsername = new Dictionary<string, List<string>>();
+            Dictionary<string, int> FollowingsByUsername = new Dictionary<string, int>();
+
+            string input = Console.ReadLine();
+
+            while (input != "Statistics")
+            {
+                string[] command = input
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string vloggerName = command[0];
+                string action = command[1];
+
+                if (action == "joined")
+                {
+                    if (!followersByUsername.ContainsKey(vloggerName))
+                    {
+                        followersByUsername.Add(vloggerName, new List<string>());
+                        FollowingsByUsername.Add(vloggerName, 0);
+                    }
+                }
+                else if (action == "followed")
+                {
+                    string followedVlogger = command[2];
+
+                    if (followersByUsername.ContainsKey(vloggerName) 
+                        && followersByUsername.ContainsKey(followedVlogger)
+                        && vloggerName != followedVlogger
+                        && !followersByUsername[followedVlogger].Contains(vloggerName))
+                    {
+                        followersByUsername[followedVlogger].Add(vloggerName);
+                        FollowingsByUsername[vloggerName]++;
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"The V-Logger has a total of {followersByUsername.Count} vloggers in its logs.");
+
+            int counter = 1;
+
+            foreach (var kvp in followersByUsername
+                .OrderByDescending(f => f.Value.Count)
+                . ThenBy(x => FollowingsByUsername[x.Key]))
+            {
+                if (counter == 1)
+                {
+                    Console.WriteLine($"{counter}. {kvp.Key} : {kvp.Value.Count} followers," +
+                                      $" {FollowingsByUsername[kvp.Key]} following");
+
+                    if (kvp.Value.Count > 0)
+                    {
+                        foreach (var follower in kvp.Value.OrderBy(x => x))
+                        {
+                            Console.WriteLine($"*  {follower}");
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"{counter}. {kvp.Key} : {kvp.Value.Count} followers, {FollowingsByUsername[kvp.Key]} following");
+                }
+
+                counter++;
+            }
+        }
+    }
+}
---
+++ Sets and Dictionaries Advanced/EX  08. Ranking/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EX__08._Ranking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> contestsAndPasswords = new Dictionary<string, string>();
+            SortedDictionary<string, Dictionary<string, int>> pointsByStudents =
+                new SortedDictionary<string, Dictionary<string, int>>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "end of contests")
+                {
+                    break;
+                }
+
+                string[] contestData = input
+                    .Split(':', StringSplitOptions.RemoveEmptyEntries);
+
+                string contest = contestData[0];
+                string password = contestData[1];
+
+                contestsAndPasswords.Add(contest, password);
+            }
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "end of submissions")
+                {
+                    break;
+                }
+
+                string[] studentsData = input
+                    .Split("=>", StringSplitOptions.RemoveEmptyEntries);
+
+                string contest = studentsData[0];
+                string password = studentsData[1];
+                string name = studentsData[2];
+                int points = int.Parse(studentsData[3]);
+
+                if (contestsAndPasswords.ContainsKey(contest) && contestsAndPasswords.ContainsValue(password))
+                {
+                    if (pointsByStudents.ContainsKey(name))
+                    {
+                        if (pointsByStudents[name].ContainsKey(contest)
+                            && pointsByStudents[name][contest] < points)
+                        {
+                            pointsByStudents[name][contest] = points;
+                        }
+
+                        if (!pointsByStudents[name].ContainsKey(contest))
+                        {
+                            pointsByStudents[name].Add(contest, points);
+                        }
+                    }
+                    else
+                    {
+                        pointsByStudents.Add(name, new Dictionary<string, int>() { { contest, points } });
+                    }
+                }
+            }
+
+            foreach (var kvp in pointsByStudents
+                .OrderByDescending(x => x.Value.Values.Sum())
+                .Take(1))
+            {
+                Console.WriteLine($"Best candidate is {kvp.Key} with total {kvp.Value.Values.Sum()} points.");
+            }
+            Console.WriteLine("Ranking:");
+            foreach (var kvp in pointsByStudents)
+            {
+                Console.WriteLine($"{kvp.Key}");
+                foreach (var contest in kvp.Value.OrderByDescending(x=> x.Value))
+                {
+                    Console.WriteLine($"#  {contest.Key} -> {contest.Value}");
+                }
+            }
+        }
+    }
+}
