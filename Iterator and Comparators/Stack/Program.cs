1b75ff233cbbacf296552f7452a1b0127795a135
Plamen Angelov <plamen565@gmail.com>
2021-06-24 09:48:35 +0300

    Iterators and Comparators
    Add Stack problem

---
+++ Iterator and Comparators/Stack/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+namespace Stack
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> stack = new Stack<string>();
+            string command = Console.ReadLine();
+
+            while (command != "END")
+            {
+                string[] tokens = command
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                if (tokens[0] == "Push")
+                {
+                    string[] data = tokens
+                        .Skip(1)
+                        .Select(e => e.Split(',').First())
+                        .ToArray();
+
+                    stack.Push(data);
+                }
+                else if (tokens[0] == "Pop")
+                {
+                    try
+                    {
+                        stack.Pop();
+
+                    }
+                    catch (ArgumentException ae)
+                    {
+
+                        Console.WriteLine(ae.Message);
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
